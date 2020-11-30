8d45c2ce3647e97840c76faa55c42b94f6de9eb5
Alex Burgoon <alex.burgoon@hmdlabs.io>
2020-11-30 10:12:28 -0500

    fix: :bug: add build_type_class to __init__.py

--- src/python/hmd_meta_types/__init__.py
+++ src/python/hmd_meta_types/__init__.py
@@ -1,4 +1,4 @@
 from .metatypes import Noun
 from .utils import load_definition, load_definition_file
-from .hmd_meta_types import extends, operation
+from .hmd_meta_types import extends, operation, build_type_class
 from .primitives import Attribute
