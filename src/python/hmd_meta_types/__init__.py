89617ccaa9cad88a0d5cd77e9c44db6580345324
Alex Burgoon <alex.burgoon@hmdlabs.io>
2020-12-01 16:38:05 -0500

    refactor: :recycle: add Reference to main __init__

--- src/python/hmd_meta_types/__init__.py
+++ src/python/hmd_meta_types/__init__.py
@@ -1,4 +1,4 @@
 from .metatypes import Noun, Extension
 from .utils import load_definition, load_definition_file
 from .hmd_meta_types import operation, build_type_class
-from .primitives import Attribute
+from .primitives import Attribute, Reference
