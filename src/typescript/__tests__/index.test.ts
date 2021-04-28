d5ea4b074e1c2beb4b5ab97939fc1aa5dc2bff83
Alex Burgoon <alex.burgoon@hmdlabs.io>
2021-04-28 11:37:13 -0400

    feat: :sparkles: adds timestamp/blob support

--- src/typescript/__tests__/index.test.ts
+++ src/typescript/__tests__/index.test.ts
@@ -1,4 +1,16 @@
-import { EntityDefinition, EntityInput, Noun } from '../src/index';
+import {
+  EntityDefinition,
+  EntityData,
+  Noun,
+  nounFactory,
+  Timestamp,
+  parseTimestamp,
+  Mapping,
+  Collection,
+  Relationship,
+  Entity,
+  relationshipFactory,
+} from '../src/index';
 
 interface ANounDefinition extends EntityDefinition {
   name: 'a_noun';
@@ -15,26 +27,36 @@ interface ANounDefinition extends EntityDefinition {
   };
 }
 
-class ANoun extends Noun {
-  static entity_definition: ANounDefinition = {
-    name: 'a_noun',
-    namespace: 'name.space',
-    metatype: 'noun',
-    attributes: {
-      field1: { type: 'string', required: true },
-      field2: { type: 'integer' },
-      field3: { type: 'enum', enum_def: ['a', 'b'] },
-      timestampfield: { type: 'timestamp' },
-      dictfield: { type: 'mapping' },
-      listfield: { type: 'collection' },
-      blobfield: { type: 'blob' },
-    },
-  };
-  data: EntityInput<ANoun>;
+const entity_definition: ANounDefinition = {
+  name: 'a_noun',
+  namespace: 'name.space',
+  metatype: 'noun',
+  attributes: {
+    field1: { type: 'string', required: true },
+    field2: { type: 'integer' },
+    field3: { type: 'enum', enum_def: ['a', 'b'] },
+    timestampfield: { type: 'timestamp' },
+    dictfield: { type: 'mapping' },
+    listfield: { type: 'collection' },
+    blobfield: { type: 'blob' },
+  },
+};
 
-  constructor(obj: EntityInput<ANoun>) {
+class ANoun extends Noun {
+  static entity_definition: ANounDefinition = entity_definition;
+  data: EntityData<ANoun>;
+
+  constructor(obj: EntityData<ANoun>) {
     super(obj);
-    this.data = obj;
+    let data: any = {};
+    for (let attr in ANoun.entity_definition.attributes) {
+      let value = this._setter(
+        attr,
+        obj[attr as keyof EntityData<ANoun>],
+      );
+      data[attr] = value;
+    }
+    this.data = data;
   }
 
   entityDefinition() {
@@ -46,7 +68,7 @@ class ANoun extends Noun {
   }
 
   public set field1(v: string) {
-    this.data.field1 = v;
+    this.data.field1 = this._setter('field1', v);
   }
 
   public get field2(): number | undefined {
@@ -54,7 +76,7 @@ class ANoun extends Noun {
   }
 
   public set field2(v: number | undefined) {
-    this.data.field2 = v;
+    this.data.field2 = this._setter('field2', v);
   }
 
   public get field3(): 'a' | 'b' | undefined {
@@ -62,31 +84,31 @@ class ANoun extends Noun {
   }
 
   public set field3(v: 'a' | 'b' | undefined) {
-    this.field3 = v;
+    this.field3 = this._setter('field3', v);
   }
 
-  public get timestampfield(): string | undefined {
+  public get timestampfield(): Timestamp | undefined {
     return this.data.timestampfield;
   }
 
-  public set timestampfield(v: string | undefined) {
-    this.data.timestampfield = v;
+  public set timestampfield(v: Timestamp | undefined) {
+    this.data.timestampfield = this._setter('timestampfield', v);
   }
 
-  public get dictfield(): { [k: string]: any } | undefined {
+  public get dictfield(): Mapping | undefined {
     return this.data.dictfield;
   }
 
-  public set dictfield(v: { [k: string]: any } | undefined) {
-    this.data.dictfield = v;
+  public set dictfield(v: Mapping | undefined) {
+    this.data.dictfield = this._setter('dictfield', v);
   }
 
-  public get listfield(): any[] | undefined {
+  public get listfield(): Collection | undefined {
     return this.data.listfield;
   }
 
-  public set listfield(v: any[] | undefined) {
-    this.data.listfield = v;
+  public set listfield(v: Collection | undefined) {
+    this.data.listfield = this._setter('listfield', v);
   }
 
   public get blobfield(): string | undefined {
@@ -94,10 +116,76 @@ class ANoun extends Noun {
   }
 
   public set blobfield(v: string | undefined) {
-    this.data.blobfield = v;
+    this.data.blobfield = this._setter('blobfield', v);
   }
 }
 
+interface ARelationshipDefinition extends EntityDefinition {
+  attributes: {
+    field1: { type: 'string'; required: true };
+    field2: { type: 'integer' };
+    field3: { type: 'enum'; enum_def: ['a', 'b'] };
+  };
+}
+const rel_definition: ARelationshipDefinition = {
+  name: 'a_relationship',
+  namespace: 'name.space',
+  metatype: 'relationship',
+  attributes: {
+    field1: { type: 'string', required: true },
+    field2: { type: 'integer' },
+    field3: { type: 'enum', enum_def: ['a', 'b'] },
+  },
+};
+
+class ARelationship extends Relationship<ANoun, ANoun> {
+  entityDefinition(): EntityDefinition {
+    return ARelationship.entity_definition;
+  }
+  protected data: EntityData<ARelationship>;
+  static entity_definition: ARelationshipDefinition = rel_definition;
+
+  constructor(
+    obj: EntityData<ARelationship>,
+    ref_from: ANoun,
+    ref_to: ANoun,
+  ) {
+    super(obj, ref_from, ref_to);
+    let data: any = {};
+    for (let attr in ARelationship.entity_definition.attributes) {
+      let value = this._setter(
+        attr,
+        obj[attr as keyof EntityData<ANoun>],
+      );
+      data[attr] = value;
+    }
+    this.data = data;
+  }
+
+  public get field1(): string {
+    return this.data.field1;
+  }
+
+  public set field1(v: string) {
+    this.data.field1 = this._setter('field1', v);
+  }
+
+  public get field2(): number | undefined {
+    return this.data.field2;
+  }
+
+  public set field2(v: number | undefined) {
+    this.data.field2 = this._setter('field2', v);
+  }
+
+  public get field3(): 'a' | 'b' | undefined {
+    return this.data.field3;
+  }
+
+  public set field3(v: 'a' | 'b' | undefined) {
+    this.field3 = this._setter('field3', v);
+  }
+}
 describe('Noun', () => {
   let anoun: ANoun;
   beforeAll(() => {
@@ -105,7 +193,7 @@ describe('Noun', () => {
       field1: 'test',
       field2: 2,
       field3: 'a',
-      timestampfield: '2020-01-01T00:00.000z',
+      timestampfield: '2020-01-01T00:00:00.000',
       dictfield: { test: 'any' },
       listfield: ['any', 2],
       blobfield: 'bigblob',
@@ -118,7 +206,7 @@ describe('Noun', () => {
     expect(anoun.field1).toBe('test');
     expect(anoun.field2).toBe(2);
     expect(anoun.field3).toBe('a');
-    expect(anoun.timestampfield).toBe('2020-01-01T00:00.000z');
+    expect(anoun.timestampfield).toBe('2020-01-01T00:00:00.000Z');
     expect(anoun.dictfield).toEqual({ test: 'any' });
     expect(anoun.listfield).toEqual(['any', 2]);
     expect(anoun.blobfield).toEqual('bigblob');
@@ -128,7 +216,7 @@ describe('Noun', () => {
       field1: 'test',
       field2: 2,
       field3: 'a',
-      timestampfield: '2020-01-01T00:00.000z',
+      timestampfield: '2020-01-01T00:00:00.000Z',
       dictfield: btoa(JSON.stringify({ test: 'any' })),
       listfield: btoa(JSON.stringify(['any', 2])),
       blobfield: btoa(JSON.stringify('bigblob')),
@@ -137,5 +225,230 @@ describe('Noun', () => {
   it('should set identifier', () => {
     anoun.identifier = 'test';
     expect(anoun.identifier).toBe('test');
+  });
+  it('should not accept invalid dates', () => {
+    expect(() => (anoun.timestampfield = 'test')).toThrow(
+      'Invalid date format for timestampfield',
+    );
+    anoun.timestampfield = undefined;
+    expect(anoun.timestampfield).toBeUndefined();
+  });
+  it('should convert timestamps to UTC', () => {
+    anoun.timestampfield = parseTimestamp(
+      '2021-01-01T00:00:00.000-05:00',
+    );
+    expect(anoun.timestampfield).toEqual('2021-01-01T05:00:00.000Z');
+  });
+});
+
+describe('Noun Factory', () => {
+  let genNoun: ReturnType<typeof nounFactory>;
+  beforeAll(() => {
+    genNoun = nounFactory(entity_definition, {
+      field1: 'test',
+      field2: 2,
+      field3: 'a',
+      timestampfield: '2020-01-01T00:00:00.000',
+      dictfield: { test: 'any' },
+      listfield: ['any', 2],
+      blobfield: 'bigblob',
+    });
+  });
+  it('should have namespaced name', () => {
+    expect(genNoun.namespaceName).toBe('name.space.a_noun');
+  });
+  it('should return field values', () => {
+    expect(genNoun.field1).toBe('test');
+    expect(genNoun.field2).toBe(2);
+    expect(genNoun.field3).toBe('a');
+    expect(genNoun.timestampfield).toBe('2020-01-01T00:00:00.000Z');
+    expect(genNoun.dictfield).toEqual({ test: 'any' });
+    expect(genNoun.listfield).toEqual(['any', 2]);
+    expect(genNoun.blobfield).toEqual('bigblob');
+  });
+  it('should serialize properly', () => {
+    expect(genNoun.serialize()).toEqual({
+      field1: 'test',
+      field2: 2,
+      field3: 'a',
+      timestampfield: '2020-01-01T00:00:00.000Z',
+      dictfield: btoa(JSON.stringify({ test: 'any' })),
+      listfield: btoa(JSON.stringify(['any', 2])),
+      blobfield: btoa(JSON.stringify('bigblob')),
+    });
+  });
+  it('should set identifier', () => {
+    genNoun.identifier = 'test';
+    expect(genNoun.identifier).toBe('test');
+  });
+  it('should not allow invalid types', () => {
+    expect(() => (genNoun.field2 = 'test')).toThrow(
+      'Invalid type for "field2", must be integer',
+    );
+    expect(() => (genNoun.dictfield = [])).toThrow(
+      'Field, dictfield, must be an object',
+    );
+    expect(() => (genNoun.listfield = {})).toThrow(
+      'Field, listfield, must be an array',
+    );
+    expect(() => (genNoun.field3 = 'test')).toThrow(
+      'Field, field3, must be one of a, b',
+    );
+    expect(() => (genNoun.field1 = { test: 'any' })).toThrow(
+      'Invalid type for "field1", must be string',
+    );
+  });
+  it('should not allow required fields to be undefined', () => {
+    expect(() => (genNoun.field1 = undefined)).toThrow(
+      'Required field, field1, cannot be undefined',
+    );
+  });
+  it('should set field values', () => {
+    genNoun.field1 = 'test again';
+    expect(genNoun.field1).toBe('test again');
+  });
+  it('should not allow missing fields', () => {
+    expect(() => nounFactory(entity_definition, {})).toThrow(
+      'Missing required fields: field1',
+    );
+  });
+  it('should not allow extra fields', () => {
+    expect(() =>
+      nounFactory(entity_definition, {
+        field1: 'dummy',
+        extra: 'field',
+      }),
+    ).toThrow('Extra fields found: extra');
+  });
+});
+
+describe('Relationship', () => {
+  let arel: ARelationship;
+  let a1, a2: ANoun;
+  beforeAll(() => {
+    a1 = new ANoun({
+      field1: 'test',
+      field2: 1,
+      field3: 'a',
+      timestampfield: '2020-01-01T00:00:00.000',
+      dictfield: { test: 'any' },
+      listfield: ['any', 2],
+      blobfield: 'bigblob',
+    });
+    a2 = new ANoun({
+      field1: 'test',
+      field2: 2,
+      field3: 'b',
+      timestampfield: '2020-01-01T00:00:00.000',
+      dictfield: { test: 'any' },
+      listfield: ['any', 2],
+      blobfield: 'bigblob',
+    });
+    arel = new ARelationship(
+      {
+        field1: 'test',
+        field2: 2,
+        field3: 'a',
+      },
+      a1,
+      a2,
+    );
+  });
+  it('should have namespaced name', () => {
+    expect(arel.namespaceName).toBe('name.space.a_relationship');
+  });
+  it('should return field values', () => {
+    expect(arel.field1).toBe('test');
+    expect(arel.field2).toBe(2);
+    expect(arel.field3).toBe('a');
+  });
+  it('should serialize properly', () => {
+    expect(arel.serialize()).toEqual({
+      field1: 'test',
+      field2: 2,
+      field3: 'a',
+    });
+  });
+  it('should set identifier', () => {
+    arel.identifier = 'test';
+    expect(arel.identifier).toBe('test');
+  });
+  it('should have from/to refs', () => {
+    let from = arel.refFrom;
+    let to = arel.refTo;
+
+    expect(from?.field2).toBe(1);
+    expect(to?.field2).toBe(2);
+
+    arel.refFrom = to;
+    arel.refTo = from;
+    expect(arel.refFrom?.field2).toBe(2);
+    expect(arel.refTo?.field2).toBe(1);
+  });
+});
+
+describe('Relationship Factory', () => {
+  let genRel: ReturnType<typeof relationshipFactory>;
+  let a1, a2: ANoun;
+  beforeAll(() => {
+    a1 = new ANoun({
+      field1: 'test',
+      field2: 1,
+      field3: 'a',
+      timestampfield: '2020-01-01T00:00:00.000',
+      dictfield: { test: 'any' },
+      listfield: ['any', 2],
+      blobfield: 'bigblob',
+    });
+    a2 = new ANoun({
+      field1: 'test',
+      field2: 2,
+      field3: 'b',
+      timestampfield: '2020-01-01T00:00:00.000',
+      dictfield: { test: 'any' },
+      listfield: ['any', 2],
+      blobfield: 'bigblob',
+    });
+    genRel = relationshipFactory(
+      rel_definition,
+      {
+        field1: 'test',
+        field2: 2,
+        field3: 'a',
+      },
+      a1,
+      a2,
+    );
+  });
+  it('should have namespaced name', () => {
+    expect(genRel.namespaceName).toBe('name.space.a_relationship');
+  });
+  it('should return field values', () => {
+    expect(genRel.field1).toBe('test');
+    expect(genRel.field2).toBe(2);
+    expect(genRel.field3).toBe('a');
+  });
+  it('should serialize properly', () => {
+    expect(genRel.serialize()).toEqual({
+      field1: 'test',
+      field2: 2,
+      field3: 'a',
+    });
+  });
+  it('should set identifier', () => {
+    genRel.identifier = 'test';
+    expect(genRel.identifier).toBe('test');
+  });
+  it('should have from/to refs', () => {
+    let from = genRel.refFrom;
+    let to = genRel.refTo;
+
+    expect(from?.field2).toBe(1);
+    expect(to?.field2).toBe(2);
+
+    genRel.refFrom = to;
+    genRel.refTo = from;
+    expect(genRel.refFrom?.field2).toBe(2);
+    expect(genRel.refTo?.field2).toBe(1);
   });
 });
--- src/typescript/src/index.ts
+++ src/typescript/src/index.ts
@@ -1,3 +1,12 @@
+import moment from 'moment';
+moment().utc(true);
+
+export type Timestamp = moment.Moment | string;
+export type Mapping = { [k: string]: any } | string;
+export type Collection = any[] | string;
+
+export const parseTimestamp = (timestamp: string) =>
+  moment.utc(timestamp);
 export interface AttributeDefintion {
   type:
     | 'string'
@@ -19,16 +28,22 @@ type EntityAttributes = { [k: string]: AttributeDefintion };
 export type EntityDefinition = {
   name: string;
   namespace: string;
-  metatype: 'noun' | 'relationshp';
+  metatype: 'noun' | 'relationship';
   attributes: EntityAttributes;
 };
 
-type AttributeType<
-  A extends AttributeDefintion
-> = A['type'] extends 'string'
+type AttributeType<A extends AttributeDefintion> = A['type'] extends
+  | 'string'
+  | 'blob'
   ? string
-  : A['type'] extends 'integer' | 'float'
+  : A['type'] extends 'integer' | 'float' | 'epoch'
   ? number
+  : A['type'] extends 'mapping'
+  ? Mapping
+  : A['type'] extends 'collection'
+  ? Collection
+  : A['type'] extends 'timestamp'
+  ? Timestamp
   : any;
 
 type RequiredAttributeKeys<D extends EntityDefinition> = {
@@ -56,18 +71,81 @@ type EntityType<D extends EntityDefinition> = {
     >;
   };
 
-export type EntityInput<D extends Entity> = EntityType<
+export type EntityData<D extends Entity> = EntityType<
   ReturnType<D['entityDefinition']>
 >;
 
+const validateAttributeValue = (
+  attr: string,
+  attribute: AttributeDefintion,
+  value: any,
+) => {
+  const typemap = {
+    string: ['string'],
+    integer: ['number'],
+    float: ['number'],
+    enum: ['string'],
+    mapping: ['object', 'string'],
+    collection: ['object', 'string'],
+    blob: ['string'],
+    timestamp: ['object', 'string'],
+    epoch: ['number'],
+  };
+
+  if (value === undefined && attribute.required) {
+    throw new Error(`Required field, ${attr}, cannot be undefined`);
+  } else if (value === undefined && attribute.required !== true) {
+    return value;
+  }
+  if (typemap[attribute.type].indexOf(typeof value) <= -1) {
+    throw new Error(
+      `Invalid type for "${attr}", must be ${attribute.type}`,
+    );
+  }
+  if (attribute.type === 'collection') {
+    if (typeof value === 'string') value = atob(value);
+    if (!Array.isArray(value))
+      throw new Error(`Field, ${attr}, must be an array`);
+  }
+  if (attribute.type === 'mapping') {
+    if (typeof value === 'string') value = atob(value);
+    if (typeof value !== 'object' || Array.isArray(value))
+      throw new Error(`Field, ${attr}, must be an object`);
+  }
+  if (
+    attribute.type === 'enum' &&
+    attribute.enum_def?.indexOf(value) === -1
+  ) {
+    throw new Error(
+      `Field, ${attr}, must be one of ${attribute.enum_def.join(
+        ', ',
+      )}`,
+    );
+  }
+  if (attribute.type === 'timestamp') {
+    if (!moment(value).isValid())
+      throw new Error(`Invalid date format for ${attr}`);
+    return parseTimestamp(value).toISOString();
+  }
+
+  return value;
+};
 export abstract class Entity {
   _identifier: string | undefined;
-  constructor(obj: EntityInput<Entity>) {
+  constructor(obj: EntityData<Entity>) {
     this._identifier = obj.identifier;
   }
 
   abstract entityDefinition(): EntityDefinition;
-  protected abstract data: EntityInput<Entity>;
+  protected abstract data: EntityData<Entity>;
+
+  protected _setter(attr: string, value: any) {
+    return validateAttributeValue(
+      attr,
+      this.entityDefinition().attributes[attr],
+      value,
+    );
+  }
 
   public get identifier(): string | undefined {
     return this._identifier;
@@ -84,7 +162,7 @@ export abstract class Entity {
     return `${namespace !== undefined ? namespace + '.' : ''}${name}`;
   }
 
-  public serialize(): EntityInput<Entity> {
+  public serialize(): EntityData<Entity> {
     const entity_definition = this.entityDefinition();
     let result: any = {};
 
@@ -92,7 +170,7 @@ export abstract class Entity {
 
     for (let attr in entity_definition.attributes) {
       let attrDef = entity_definition.attributes[attr];
-      let value: any = this.data[attr as keyof EntityInput<Entity>];
+      let value: any = this.data[attr as keyof EntityData<Entity>];
 
       if (value != null) {
         if (
@@ -100,11 +178,11 @@ export abstract class Entity {
         ) {
           value = btoa(JSON.stringify(value));
         }
-        result[attr as keyof EntityInput<Entity>] = value;
+        result[attr as keyof EntityData<Entity>] = value;
       }
     }
 
-    return result as EntityInput<Entity>;
+    return result as EntityData<Entity>;
   }
 }
 
@@ -113,33 +191,187 @@ export abstract class Noun extends Entity {
   from_rels: { [k: string]: Relationship<Noun, Noun>[] } = {};
 }
 
-export abstract class Relationship<TFrom, TTo> extends Entity {
-  private _ref_from: TFrom;
-  private _ref_to: TTo;
+export abstract class Relationship<
+  TFrom extends Noun = Noun,
+  TTo extends Noun = Noun
+> extends Entity {
+  private _ref_from?: TFrom;
+  private _ref_to?: TTo;
 
   constructor(
-    obj: EntityInput<Relationship<TFrom, TTo>>,
-    ref_from: TFrom,
-    ref_to: TTo,
+    obj: EntityData<Relationship<TFrom, TTo>>,
+    ref_from?: TFrom,
+    ref_to?: TTo,
   ) {
     super(obj);
     this._ref_from = ref_from;
     this._ref_to = ref_to;
   }
 
-  public get ref_from(): TFrom {
+  public get refFrom(): TFrom | undefined {
     return this._ref_from;
   }
 
-  public set ref_from(v: TFrom) {
+  public set refFrom(v: TFrom | undefined) {
     this._ref_from = v;
   }
 
-  public get ref_to(): TTo {
+  public get refTo(): TTo | undefined {
     return this._ref_to;
   }
 
-  public set ref_to(v: TTo) {
+  public set refTo(v: TTo | undefined) {
     this._ref_to = v;
   }
+}
+
+export function nounFactory(
+  definition: EntityDefinition,
+  obj: EntityType<EntityDefinition>,
+) {
+  const validateObj = (obj: EntityType<EntityDefinition>) => {
+    const attributes = new Set(Object.keys(definition.attributes));
+    const requiredFields = new Set(
+      Object.keys(definition.attributes).filter(
+        (attr) => definition.attributes[attr].required === true,
+      ),
+    );
+    const objFields = new Set(Object.keys(obj));
+
+    const missingFields = [...Array.from(requiredFields)].filter(
+      (v) => !objFields.has(v),
+    );
+
+    if (missingFields.length > 0)
+      throw new Error(
+        `Missing required fields: ${missingFields.join(', ')}`,
+      );
+
+    const extraFields = [...Array.from(objFields)].filter(
+      (v) => !attributes.has(v),
+    );
+
+    if (extraFields.length > 0)
+      throw new Error(
+        `Extra fields found: ${extraFields.join(', ')}`,
+      );
+    let data: any = {};
+    for (let attr of Array.from(attributes)) {
+      let value = validateAttributeValue(
+        attr,
+        definition.attributes[attr],
+        obj[attr as keyof EntityType<EntityDefinition>],
+      );
+      data[attr] = value;
+    }
+
+    return data as EntityType<EntityDefinition>;
+  };
+
+  class GenericNoun extends Noun {
+    entityDefinition(): EntityDefinition {
+      return definition;
+    }
+    protected data: EntityType<EntityDefinition>;
+
+    constructor(obj: EntityType<EntityDefinition>) {
+      super(obj);
+      this.data = validateObj(obj);
+
+      for (let attr in this.data) {
+        Object.defineProperty(this, attr, {
+          get: () =>
+            this.data[attr as keyof EntityType<EntityDefinition>],
+          set: (value: any) => {
+            this.data[
+              attr as keyof EntityType<EntityDefinition>
+            ] = this._setter(attr, value);
+          },
+        });
+      }
+    }
+  }
+
+  const noun: unknown = new GenericNoun(obj);
+  return noun as Noun & EntityType<EntityDefinition>;
+}
+
+export function relationshipFactory<
+  TFrom extends Noun = Noun,
+  TTo extends Noun = Noun
+>(
+  definition: EntityDefinition,
+  obj: EntityType<EntityDefinition>,
+  refFrom?: TFrom,
+  refTo?: TTo,
+) {
+  const validateObj = (obj: EntityType<EntityDefinition>) => {
+    const attributes = new Set(Object.keys(definition.attributes));
+    const requiredFields = new Set(
+      Object.keys(definition.attributes).filter(
+        (attr) => definition.attributes[attr].required === true,
+      ),
+    );
+    const objFields = new Set(Object.keys(obj));
+
+    const missingFields = [...Array.from(requiredFields)].filter(
+      (v) => !objFields.has(v),
+    );
+
+    if (missingFields.length > 0)
+      throw new Error(
+        `Missing required fields: ${missingFields.join(', ')}`,
+      );
+
+    const extraFields = [...Array.from(objFields)].filter(
+      (v) => !attributes.has(v),
+    );
+
+    if (extraFields.length > 0)
+      throw new Error(
+        `Extra fields found: ${extraFields.join(', ')}`,
+      );
+    let data: any = {};
+    for (let attr of Array.from(attributes)) {
+      let value = validateAttributeValue(
+        attr,
+        definition.attributes[attr],
+        obj[attr as keyof EntityType<EntityDefinition>],
+      );
+      data[attr] = value;
+    }
+
+    return data as EntityType<EntityDefinition>;
+  };
+
+  class GenericRelationship extends Relationship<TFrom, TTo> {
+    entityDefinition(): EntityDefinition {
+      return definition;
+    }
+    protected data: EntityType<EntityDefinition>;
+
+    constructor(
+      obj: EntityType<EntityDefinition>,
+      from?: TFrom,
+      to?: TTo,
+    ) {
+      super(obj, from, to);
+      this.data = validateObj(obj);
+
+      for (let attr in this.data) {
+        Object.defineProperty(this, attr, {
+          get: () =>
+            this.data[attr as keyof EntityType<EntityDefinition>],
+          set: (value: any) => {
+            this.data[
+              attr as keyof EntityType<EntityDefinition>
+            ] = this._setter(attr, value);
+          },
+        });
+      }
+    }
+  }
+
+  const noun: unknown = new GenericRelationship(obj, refFrom, refTo);
+  return noun as Noun & EntityType<EntityDefinition>;
 }
