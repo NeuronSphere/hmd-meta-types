88e7fcb6d0f5ec15c0b929032e0852ef07acb94e
Adam Stortz <adam.stortz@hmdlabs.io>
2021-06-11 15:37:22 -0500

    feat: improving os agnosticism

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
+import pathlib
 
-with open("../../meta-data/VERSION", "r") as vfl:
+repo_dir = pathlib.Path(__file__).absolute().parent.parent.parent
+version_file = repo_dir / "meta-data" / "VERSION"
+
+with open(version_file, "r") as vfl:
     version = vfl.read().strip()
 
 setup(
